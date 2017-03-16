60eb5fe8dc587b120051117771b06dea297a4909
william <williamsousadasilva@gmail.com>
2017-03-16 17:23:56 -0300

    Initial commit

---
+++ src/main/java/com/algaworks/brewer/config/init/AppInitialiazer.java
@@ -0,0 +1,37 @@
+package com.algaworks.brewer.config.init;
+
+import com.algaworks.brewer.config.WebConfig;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+/**
+ * Created by william on 16/03/2017.
+ */
+public class AppInitialiazer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[] { WebConfig.class };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+
+        return new Filter[] { characterEncodingFilter };
+    }
+}
---
+++ src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -0,0 +1,44 @@
+package com.algaworks.brewer.model;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.validation.constraints.Size;
+
+/**
+ * Created by william on 16/03/2017.
+ */
+public class Cerveja {
+
+    @NotBlank
+    private String sku;
+
+    @NotBlank
+    private String descricao;
+
+    @Size(min = 1, max = 50)
+    private String nome;
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
